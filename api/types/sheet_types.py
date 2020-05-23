411069264d49769db570c0fb73a12a808a5673af
Canchola <UA56722@unisource.corp>
2020-05-22 23:42:04 -0700

    Made sheet type hashable

--- api/types/sheet_types.py
+++ api/types/sheet_types.py
@@ -5,6 +5,9 @@ class SheetType:
         self.name = name
         self.description = description
 
+    def __hash__(self):
+        return hash(self.index)
+
 
 SCALAR_INPUT = SheetType(
     0, "SCALAR_INPUT", "Tab made up entirely from scalar inputs")
