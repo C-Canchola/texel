2932d0659ba7d68bbc657d89ffe4276f6cd41a8f
Canchola <UA56722@unisource.corp>
2020-05-23 00:35:04 -0700

    Added functions to aid in determining what the current names should be for standard row sheets

--- api/name_manager/range_getters.py
+++ api/name_manager/range_getters.py
@@ -5,11 +5,24 @@ import texel.api.types.sheet_types as txl_sht_types
 from functools import wraps
 
 SCALAR_INPUT = txl_sht_types.SCALAR_INPUT
-VALID_TYPES = [SCALAR_INPUT]
+STANDARD_ROW_OPERATION = txl_sht_types.STANDARD_ROW_OPERATION
+
+VALID_TYPES = [SCALAR_INPUT, STANDARD_ROW_OPERATION]
+
+NAMED_RANGE_NAME_FUNCS = {}
+NAMED_RANGE_ADDRESS_FUNCS = {}
 
 
+def register_type_func(sht_type, func_dict):
 
-def stop_if_empty(func):
+    def decorator(func):
+        func_dict[sht_type] = func
+        return func
+
+    return decorator
+
+
+def stop_if_row_empty(func):
 
     @wraps(func)
     def inner_func(sht: xw.Sheet):
@@ -22,7 +35,20 @@ def stop_if_empty(func):
     return inner_func
 
 
-@stop_if_empty
+def stop_if_col_empty(func):
+
+    @wraps(func)
+    def inner_func(sht: xw.Sheet):
+        if sht.range('A1').value is None:
+            return []
+
+        return func(sht)
+
+    return inner_func
+
+
+@register_type_func(SCALAR_INPUT, NAMED_RANGE_ADDRESS_FUNCS)
+@stop_if_row_empty
 def get_scalar_ranges_to_name(sht: xw.Sheet):
     """Returns a list containing the address of each filled row in the value (2nd) column
     of a scalar sheet.
@@ -36,7 +62,8 @@ def get_scalar_ranges_to_name(sht: xw.Sheet):
     return ['{}!$B${}'.format(sht_nm, i) for i in range(2, stop)]
 
 
-@stop_if_empty
+@register_type_func(SCALAR_INPUT, NAMED_RANGE_NAME_FUNCS)
+@stop_if_row_empty
 def get_scalar_names(sht: xw.Sheet):
     """Returns a list of 
 
@@ -52,3 +79,27 @@ def get_scalar_names(sht: xw.Sheet):
     return ['{}__{}'.format(sht_nm, value) for value in values]
 
 
+@register_type_func(STANDARD_ROW_OPERATION, NAMED_RANGE_ADDRESS_FUNCS)
+@stop_if_col_empty
+def get_standard_row_ranges_to_name(sht: xw.Sheet):
+
+    stop = len(sht.range('A1').current_region.columns) + 1
+    sht_nm = sht.name
+
+    return ['{sht_nm}!${col_letter}:${col_letter}'.format(
+            sht_nm=sht_nm, col_letter=xw.utils.col_name(i))
+            for i in range(1, stop)]
+
+
+@register_type_func(STANDARD_ROW_OPERATION, NAMED_RANGE_NAME_FUNCS)
+@stop_if_col_empty
+def get_standard_row_names(sht: xw.Sheet):
+
+    values = sht.range('A1').current_region[0, ::].options(ndim=1).value
+    sht_nm = sht.name
+
+    return ['{}__{}'.format(sht_nm, value) for value in values]
+
+
+def get_names_and_addresses(sht: xw.Sheet, sht_type: txl_sht_types.SheetType):
+    return NAMED_RANGE_NAME_FUNCS[sht_type](sht), NAMED_RANGE_ADDRESS_FUNCS[sht_type](sht)
--- api/types/sheet_types.py
+++ api/types/sheet_types.py
@@ -11,3 +11,7 @@ class SheetType:
 
 SCALAR_INPUT = SheetType(
     0, "SCALAR_INPUT", "Tab made up entirely from scalar inputs")
+
+STANDARD_ROW_OPERATION = SheetType(
+    1, 'STANDARD_ROW_OPERATION', 'Tab made up of columns where each calculation is deteremined by some data in the same row.'
+)
